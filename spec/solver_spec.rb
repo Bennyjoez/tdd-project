7fece95d159add3d03604936ce0bf82790556401
Benson Njuguna <njugunab655@gmail.com>
2023-07-06 18:07:57 +0300

    Fix linter errors

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,20 +1,22 @@
-require '../solver'
+require_relative '../solver'
 
 describe Solver do
-  solve = Solver.new
+  before :each do
+    @solve = Solver.new
+  end
 
-  describe "#factorial" do
-    it "Should raise an exception error for negative numbers" do
-      expect { perform_operation }.to raise_error(StandardError, "Not a positive integer")
+  describe '#factorial' do
+    it 'Should raise an exception error for negative numbers' do
+      expect { @solve.factorial(-1) }.to raise_error(ArgumentError, 'num should be an integer')
     end
 
-    it "Should return 1 for a factorial of 0 or 1" do
-      expect(solve.factorial(0)).to eql(1)
-      expect(solve.factorial(1)).to eql(1)
+    it 'Should return 1 for a factorial of 0 or 1' do
+      expect(@solve.factorial(0)).to eql(1)
+      expect(@solve.factorial(1)).to eql(1)
     end
 
-    it "Should return a value for positive integers" do
-      expect(solve.factorial(5)).to eql(120)
+    it 'Should return a value for positive integers' do
+      expect(@solve.factorial(5)).to eql(120)
     end
   end
 end
