bfdf9830f899d63e0126a034655c9f2b4e413901
Benson Njuguna <njugunab655@gmail.com>
2023-07-06 18:07:35 +0300

    Add solver factorial method

---
+++ solver.rb
@@ -0,0 +1,14 @@
+class Solver
+  def factorial(num)
+    raise ArgumentError, 'num should be an integer' if num.negative?
+
+    factorial = 1
+    return factorial if num.zero?
+
+    (1..num).each do |i|
+      factorial *= i
+    end
+
+    factorial
+  end
+end
